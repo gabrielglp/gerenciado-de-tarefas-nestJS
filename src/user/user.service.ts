a568a66406d688590cff5dc00240904db0544c4a
gabrielglp <gabriel_pg08@hotmail.com>
2024-07-31 13:50:31 -0300

    feat: error handling if the email is already in use

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
@@ -8,6 +8,14 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const userExists = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+
+    if (userExists) {
+      throw new HttpException('Email já está em uso', HttpStatus.CONFLICT);
+    }
+
     const data = {
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10),
